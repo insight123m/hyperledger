bef3727d69bcbeeb0b802240a3c13bb1f6d5be57
Insight 123M <insight123m@gmail.com>
2017-10-15 22:12:48 +0530

    Updated API endpoints

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public ApiIP: string = "http://localhost";
-    public ApiPort: string = "3000";
+    public ApiIP: string = "http://168.1.145.226";
+    public ApiPort: string = "31090";
     public Server: string = this.ApiIP+":"+this.ApiPort;
     public ApiUrl: string = "/api/";
     public ServerWithApiUrl = this.Server + this.ApiUrl;
